fe140554fbdee4c88e2ec437e0ce2819074f84be
Florian Schweizer <post.an.flo@gmail.com>
2021-12-17 17:14:08 +0100

    Change constraint to Encodable

--- Firestore/Swift/Source/PropertyWrapper/FirestoreQuery.swift
+++ Firestore/Swift/Source/PropertyWrapper/FirestoreQuery.swift
@@ -195,7 +195,7 @@ public struct FirestoreQuery<T>: DynamicProperty {
     }
     
     /// Adds a given document to Firestore
-    public func add(_ document: T) where T: Decodable {
+    public func add(_ document: T) where T: Encodable {
         firestoreQueryObservable.add(document)
     }
 }
