1ae902b2482de0db5d89d7cd0f56d576b62db726
Florian Schweizer <post.an.flo@gmail.com>
2021-12-17 17:11:27 +0100

    Add generic constraint

--- Firestore/Swift/Source/PropertyWrapper/FirestoreQuery.swift
+++ Firestore/Swift/Source/PropertyWrapper/FirestoreQuery.swift
@@ -195,7 +195,7 @@ public struct FirestoreQuery<T>: DynamicProperty {
     }
     
     /// Adds a given document to Firestore
-    public func add(_ document: U) {
+    public func add(_ document: T) where T: Decodable {
         firestoreQueryObservable.add(document)
     }
 }
