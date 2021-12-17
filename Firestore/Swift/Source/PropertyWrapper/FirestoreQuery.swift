3fcbd7493f648315d9e58ade1a90ae6eb49a5f6b
Florian Schweizer <post.an.flo@gmail.com>
2021-12-17 17:01:00 +0100

    Fix typo

--- Firestore/Swift/Source/PropertyWrapper/FirestoreQuery.swift
+++ Firestore/Swift/Source/PropertyWrapper/FirestoreQuery.swift
@@ -195,7 +195,7 @@ public struct FirestoreQuery<T>: DynamicProperty {
     }
     
     /// Adds a given document to Firestore
-    public func add(_ document: T) {
+    public func add(_ document: U) {
         firestoreQueryObservable.add(document)
     }
 }
